ebbcf4de5fc4f59852b52440fc36c5f667ecf541
dongeronimo <luciano.geronimo.fnord@gmail.com>
2017-07-23 21:35:22 -0300

    Erro no glDrawArrays().

---
+++ rendering3d.cpp
@@ -0,0 +1,186 @@
+#include "rendering3d.h"
+#include <fstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+std::string Shader::ReadShaderFile(std::string path)
+{
+	std::string VertexShaderCode;
+	std::ifstream VertexShaderStream(path.c_str(), std::ios::in);
+	if (VertexShaderStream.is_open()) {
+		std::string Line = "";
+		while (getline(VertexShaderStream, Line))
+			VertexShaderCode += "\n" + Line;
+		VertexShaderStream.close();
+	}
+	else
+	{
+		throw std::exception("arquivo não foi aberto");
+	}
+	return VertexShaderCode;
+}
+
+void Shader::introspectProgram(GLuint programId, map<string, GLuint>& attributes, map<string, GLuint>& uniforms)
+{
+	GLint numberOfAttributes, largestAttributeName;
+	glGetProgramiv(programId, GL_ACTIVE_ATTRIBUTES, &numberOfAttributes);
+	glGetProgramiv(programId, GL_ACTIVE_ATTRIBUTE_MAX_LENGTH, &largestAttributeName);
+	//para cada atributo, pegar as propriedades e guardar.
+	for (GLint i = 0; i < numberOfAttributes; i++)
+	{
+		char* nameBuffer = new char[largestAttributeName];
+		GLsizei length;
+		GLint size;
+		GLenum type;
+		//Pega, entre outras coisas, o nome do atributo.
+		glGetActiveAttrib(programId, i, largestAttributeName, &length, &size, &type, nameBuffer);
+		GLint attribLocation = glGetAttribLocation(programId, nameBuffer);
+		std::string _name(nameBuffer);
+		attributes.insert(std::make_pair(_name, attribLocation));
+	}
+	//Agora pra uniforms
+	GLint numberOfUniforms, largesUniformName;
+	glGetProgramiv(programId, GL_ACTIVE_UNIFORMS, &numberOfUniforms);
+	glGetProgramiv(programId, GL_ACTIVE_UNIFORM_MAX_LENGTH, &largesUniformName);
+	//para cada atributo, pegar as propriedades e guardar.
+	for (GLint i = 0; i < numberOfUniforms; i++)
+	{
+		char* buffer = new char[largesUniformName];
+		GLsizei length;
+		GLint size;
+		GLenum type;
+		glGetActiveUniform(programId, i, largesUniformName, &length,
+			&size, &type, buffer);
+		GLint unifLoc = glGetUniformLocation(programId, buffer);
+		std::string _name(buffer);
+		uniforms.insert(std::make_pair(_name, unifLoc));
+	}
+}
+
+string Shader::GetShaderInfoLog(GLuint object, PFNGLGETSHADERIVPROC glGet__iv, PFNGLGETSHADERINFOLOGPROC glGet__InfoLog)
+{
+	GLint log_length;
+	char *log;
+	glGet__iv(object, GL_INFO_LOG_LENGTH, &log_length);
+	log = static_cast<char*>(malloc(log_length));
+	glGet__InfoLog(object, log_length, nullptr, log);
+	return log;
+}
+
+GLuint Shader::MakeShader(GLenum type, std::string source)
+{
+	GLint length[] = { source.length() };
+	const GLchar *srcLn = source.c_str();
+	GLuint shader;
+	GLint shader_ok;
+	shader = glCreateShader(type);
+	glShaderSource(shader, 1, &srcLn, length);
+	glCompileShader(shader);
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &shader_ok);
+	if (!shader_ok)
+	{
+		string _probl = GetShaderInfoLog(shader, glGetShaderiv, glGetShaderInfoLog);
+		glDeleteShader(shader);
+		throw std::runtime_error(_probl.c_str());
+	}
+	else
+	{
+		return shader;
+	}
+}
+
+GLuint Shader::MakeProgram(GLuint vertex_shader, GLuint fragment_shader)
+{
+	GLint program_ok;
+	GLuint program = glCreateProgram();
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+	glLinkProgram(program);
+	glGetProgramiv(program, GL_LINK_STATUS, &program_ok);
+	if (!program_ok) {
+		string _probl = GetShaderInfoLog(program, glGetProgramiv, glGetProgramInfoLog);
+		glDeleteProgram(program);
+		throw std::runtime_error(_probl.c_str());
+	}
+	return program;
+}
+
+Shader::Shader(string vsPath, string fsPath)
+{
+	std::string vsSrc = ReadShaderFile(vsPath);
+	vsId = MakeShader(GL_VERTEX_SHADER, vsSrc);
+	std::string fsSrc = ReadShaderFile(fsPath);
+	fsId = MakeShader(GL_FRAGMENT_SHADER, fsSrc);
+	programId = MakeProgram(vsId, fsId);
+	introspectProgram(programId, attributes, uniforms);
+}
+
+Shader::~Shader()
+{
+	glDeleteShader(vsId);
+	glDeleteShader(fsId);
+	glDeleteShader(programId);
+}
+
+GLuint Shader::GetUniform(string name)
+{
+	return uniforms.at(name);
+}
+
+GLuint Shader::GetAttribute(string name)
+{
+	return attributes.at(name);
+}
+
+GLuint Shader::GetProgramId()
+{
+	return programId;
+}
+
+void Shader::UseProgram()
+{
+	glUseProgram(this->programId);
+}
+
+Object3d::Object3d() :shader("C:\\programacao\\radon_transform_mk2_build\\Debug\\vertexShader.glsl", 
+	"C:\\programacao\\radon_transform_mk2_build\\Debug\\fragmentShader.glsl")
+{
+	vertexes.push_back(-1.0f); vertexes.push_back(-1.0f); vertexes.push_back(0.0f);
+	vertexes.push_back(1.0f); vertexes.push_back(-1.0f); vertexes.push_back(0.0f);
+	vertexes.push_back(0.0f); vertexes.push_back(1.0f); vertexes.push_back(0.0f);
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexes.size()*sizeof(float)), vertexes.data(), GL_STATIC_DRAW);
+}
+
+void teste_opengl()
+{
+	GLenum err = glGetError();
+	if (err != GL_NO_ERROR)
+	{
+		std::cout << "erro de opengl em algum lugar";
+		throw err;
+	}
+}
+void Object3d::Render()
+{
+	teste_opengl();
+	shader.UseProgram();
+	teste_opengl();
+	glEnableVertexAttribArray(shader.GetAttribute("vertex"));
+	teste_opengl();
+	glBindBuffer(GL_ARRAY_BUFFER, shader.GetAttribute("vertex"));
+	teste_opengl();
+	glVertexAttribPointer(
+		shader.GetAttribute("vertex"),                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+		3,                  // size
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		0,                  // stride
+		(void*)0            // array buffer offset
+	);
+	teste_opengl();
+	glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+	teste_opengl();
+	//glDisableVertexAttribArray(0);
+}
---
+++ rendering3d.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include <gl/glew.h>
+#include <memory>
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+class Shader
+{
+private:
+	GLuint programId, vsId, fsId;
+	map<string, GLuint> attributes;
+	map<string, GLuint> uniforms;
+
+	std::string ReadShaderFile(std::string path);
+	//Extrai a lista de atributos e uniformes do programa dado. Os pares nome/localiza��o no shader s�o retornados
+	//nos mapas passados como par�metro.
+	void introspectProgram(GLuint programId, map<string, GLuint> &attributes, map<string, GLuint> &uniforms);
+	//Gera o log do shader. A responsabilidade de deletar a string alocada � do invocador da fun��o.
+	string GetShaderInfoLog(GLuint object, PFNGLGETSHADERIVPROC glGet__iv, PFNGLGETSHADERINFOLOGPROC glGet__InfoLog);
+	//Cria um shader do tipo dado a partir do c�digo fonte dado.
+	GLuint MakeShader(GLenum type, std::string source);
+	//Cria o programa de shader a partir dos shaders dados.
+	GLuint MakeProgram(GLuint vertex_shader, GLuint fragment_shader);
+public:
+	Shader(string vsPath, string fsPath);
+	~Shader();
+	GLuint GetUniform(string name);
+	GLuint GetAttribute(string name);
+	GLuint GetProgramId();
+	void UseProgram();
+};
+
+class Object3d
+{
+private:
+	GLuint vertexbuffer;
+	vector<GLfloat> vertexes;
+	Shader shader;
+public:
+	Object3d() ;
+	void Render();
+};
